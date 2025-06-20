c1742c82095c2a6ccde854e42bd58b4105562dc4
Obert-Bench <rabeamasri97@gmail.com>
2025-06-20 14:42:56 -0400

    add click

--- clock_gui.py
+++ clock_gui.py
@@ -3,6 +3,7 @@ from time import strftime
 from json import load
 
 window = Tk()
+alpha = 1
 
 f = open('config.json')
 prof = load(f)
@@ -17,7 +18,13 @@ def on_enter(event):
     window.attributes("-alpha", 0.2) 
 
 def on_leave(event):
-    window.attributes("-alpha", 1) 
+    window.attributes("-alpha", alpha) 
+
+def on_click(event):
+    print("click")
+
+def on_drag(event):
+    print("drag")
 
 #window.attributes('-toolwindow', False) #taskbar icon, doesnt work w overrideredirect
 background_color = prof["background_color"]
@@ -25,6 +32,7 @@ foreground_color = prof["foreground_color"]
 string_format = prof["string_format"]
 font_details = (prof["font_family"],prof["font_size"],prof["font_style"])
 window.geometry("+" + prof["x_position"] + "+" + prof["y_position"])
+
 if not prof["decorator_visible"]:
     window.overrideredirect(True)
 if prof["always_on_top"]:
@@ -35,12 +43,16 @@ update_interval = 6000
 if "%S" in string_format:
     update_interval = 1000
 if prof["alpha"] != 0:
-    window.attributes("-alpha", 0.7) 
+    alpha = prof["alpha"]
+
+window.attributes("-alpha", alpha) 
 
 label = Label(window, text="",font=font_details,fg=foreground_color, bg=background_color) 
 label.pack(expand=True)
 label.bind("<Enter>",on_enter)
 label.bind("<Leave>",on_leave)
+label.bind("<Button-1>", on_click)
+#label.bind("<Button-1>",on_drag)
 
 if __name__ == "__main__":
     update_label() 
