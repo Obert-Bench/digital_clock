f1053127d268018284e83cc9bf191c53c5b2f075
Obert-Bench <rabeamasri97@gmail.com>
2025-07-03 15:51:46 -0400

    start implementing settings toplevel

--- clock_gui.py
+++ clock_gui.py
@@ -1,15 +1,18 @@
-from tkinter import Tk, Label
+from tkinter import  Tk, Label, Checkbutton, Toplevel, Scale, HORIZONTAL
 from time import strftime
 from json import load
 
-# TODO: right click to display settings, drag to move gui
+# TODO: right click to display settings
 
 class DigitalClock(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
-        self.clock_label = Label( text="foooo") 
 
+        self.clock_label = Label( text="foooo") 
+        self.clock_label.pack(expand=True)
         self.update_interval = 6000
+        self.init = [0,0]               # used to adjust relative position of mouse to gui during drag event
+
         self.config = {
             "alpha": 1,
             "always_on_top": True,
@@ -29,14 +32,11 @@ class DigitalClock(Tk):
         self.set_config()
         self.update_label()
 
-        self.clock_label.pack(expand=True)
-
-        #needed to adjust relative position of mouse to gui during drag event
-        self.init = [0,0]
         self.bind("<Enter>",lambda e:self.update_alpha(e,0.2))
         self.bind("<Leave>",lambda e:self.update_alpha(e,self.config["alpha"]))
         self.bind("<Button-1>", self.on_click)
         self.bind("<B1-Motion>",self.on_drag)
+        self.bind("<Button-3>",self.open_settings)
 
     def load_config(self,file_name = "config.json"):
         try:
@@ -83,6 +83,31 @@ class DigitalClock(Tk):
         x = self.winfo_x() + (event.x)
         y = self.winfo_y() + (event.y)
         self.geometry(f"+{x - self.init[0]}+{y - self.init[1]}")
+
+    def open_settings(self,event):
+        settings = Toplevel(self)
+        settings.wm_title("Settings")
+        c = 0
+        for setting in self.config:
+            l = Label(settings, text=setting)
+            l.grid(row=c,column=0,sticky="W")
+            c+=1
+        w_alpha = Scale(settings, from_=0, to=1, orient=HORIZONTAL, resolution= 0.1)
+        w_alpha.grid(row=0,column=1)
+
+        # error
+        #var = 1
+        #w_aot = Checkbutton(setting,variable=var,onvalue=1,offvalue=0)
+        #w_aot.grid(row=1,column=1)
+
+        w_xpos = Scale(settings, from_=0, to=3000, orient=HORIZONTAL)
+        w_xpos.grid(row=5,column=1)
+        w_ypos = Scale(settings, from_=0, to=3000, orient=HORIZONTAL)
+        w_ypos.grid(row=6,column=1)
+
+        w_fsize = Scale(settings, from_=1, to=100, orient=HORIZONTAL)
+        w_fsize.grid(row=12,column=1)
+
 
     
 if __name__ == "__main__":
